d3e2f85eb7d03f7daa16fe15230e9d803915c61c
Suyash Mishra <156529215+6394413745@users.noreply.github.com>
2025-10-05 11:38:08 +0530

    Time: 6 ms (23.25%), Space: 52.3 MB (16.88%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,63 +2,60 @@ class Solution {
     class Pair{
         int row;
         int col;
-        Pair(int row, int col){
+        Pair(int row,int col){
             this.row = row;
             this.col = col;
-        }
+        } 
     }
-    public int numIslands(char[][] grid){
-        int m = grid.length;
-        int n = grid[0].length;
-    
+    public int numIslands(char[][] adj) {
+        int m = adj.length;
+        int n = adj[0].length;
+        int count =0;
         boolean[][] vis = new boolean[m][n];
-        int count = 0;
-        for(int i  = 0;i<m;i++){
+        for(int i = 0;i<m;i++){
             for(int j = 0;j<n;j++){
-               if(grid[i][j] == '1' && vis[i][j] == false){
-                   bfs(i,j,grid,vis);
-                   count++;
-               }
+                if(adj[i][j] == '1' && vis[i][j] == false){
+                    bfs(i,j,vis,adj);
+                    count++;
+                }
             }
         }
         return count;
+        
     }
-    
-    public  void bfs(int i, int j, char[][] grid, boolean[][] vis){
-        int m = grid.length;
-        int n = grid[0].length;
+    public void bfs(int i,int j, boolean[][] vis, char[][] adj){
+        int m = adj.length;
+        int n = adj[0].length;
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(i,j));
+        q.add(new Pair(i,j)); 
         while(q.size() > 0){
             Pair front = q.remove();
-            int row = front.row, col = front.col;
+            int row = front.row; int col = front.col;
 
-
-            if(row >0){
-            if(vis[row-1][col] == false && grid[row-1][col] == '1'){
-                q.add(new  Pair(row-1,col));
-                vis[row-1][col] = true;
+            if(row > 0){
+                if(vis[row-1][col] == false && adj[row-1][col] == '1'){
+                    q.add(new Pair(row-1,col));
+                    vis[row-1][col] = true;
+                }
+            }
+            if((row+1) < m){
+                if(vis[row+1][col] == false && adj[row+1][col] == '1'){
+                    q.add(new Pair(row+1,col));
+                    vis[row+1][col] = true;
+                }
+            }
+            if(col > 0){
+                if(vis[row][col-1] == false && adj[row][col-1] == '1'){
+                    q.add(new Pair(row,col-1)); 
+                    vis[row][col-1] = true;
+                }
+            }
+            if((col+1) <n){
+                if(vis[row][col+1] == false && adj[row][col+1] == '1'){
+                    q.add(new Pair(row,col+1));
+                    vis[row][col+1] = true;
+                }
             }
         }
-        if((row+1) < m){
-            if(vis[row+1][col] == false && grid[row+1][col] == '1'){
-                q.add(new  Pair(row+1,col));
-                vis[row+1][col] = true;
-            }
-        }
-        if(col > 0){
-            if(vis[row][col-1] == false && grid[row][col-1] == '1'){
-                q.add(new  Pair(row,col-1));
-                vis[row][col-1] = true;
-            }
-        }
-        if((col +1) <n){
-            if(vis[row][col+1] == false && grid[row][col+1] == '1'){
-                q.add(new  Pair(row,col+1));
-                vis[row][col+1] = true;
-            }
-        }
-        }
-
     }
 }
