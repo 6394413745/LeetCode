b0ba35ab33c9289d5ffb3ec0d66fe61bee64a051
Suyash Mishra <156529215+6394413745@users.noreply.github.com>
2025-11-01 22:56:43 +0530

    Time: 0 ms (100%), Space: 40.3 MB (66.58%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,6 +1,14 @@
 class Solution {
+    
+    public int fibo(int n,int [] dp){
+        if(n<=1)return n;
+        if(dp[n] != 0) return dp[n];//agr dp me nth element 0 nhi hai matlab value padi huyi hai to wahi return kar denge
+        int ans = fibo(n-1,dp) + fibo(n-2,dp);//agr nhi hai to call lga denge
+        dp[n] = ans;//aur call lagane ke baad  wo value dp arr me add kar denge
+        return ans;
+    }
     public int fib(int n) {
-        if(n <=1)return n;
-        return fib(n-1) + fib(n-2);
+       int [] dp = new int[n+1];//initialise the size with n+1
+       return fibo(n,dp);
     }
 }
