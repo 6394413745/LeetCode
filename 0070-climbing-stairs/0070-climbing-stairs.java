a5bac04a58bdf3b4f92c6c5547e84a76827d154f
Suyash Mishra <156529215+6394413745@users.noreply.github.com>
2025-11-02 15:00:40 +0530

    Time: 0 ms (100%), Space: 42.1 MB (5.66%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static int countWay(int n, int[] dp){
+         if(n==0 || n==1)return 1;
+         if(dp[n] != -1)return dp[n];
+         dp[n] = countWay(n-1,dp) + countWay(n-2,dp);
+         return dp[n];
+         
+    }
+    public int climbStairs(int n) {
+        int [] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return countWay(n,dp);
+    }
+}
