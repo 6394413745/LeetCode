485c87da4ed33f19e439981e1555aad30e9927a7
Suyash Mishra <156529215+6394413745@users.noreply.github.com>
2025-11-03 20:09:20 +0530

    Time: 1 ms (35.6%), Space: 44.6 MB (5.34%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,14 +1,16 @@
 class Solution {
-    public int minCost(int [] cost,int idx,int [] dp){
-        if(idx == 0 || idx == 1)return cost[idx];
-        if(dp[idx] != -1)return dp[idx];
-        return dp[idx] = cost[idx] + Math.min(minCost(cost,idx-1,dp),minCost(cost,idx-2,dp));
-        
+    public static int minCost(int i, int [] cost,int [] dp){
+        if(i==0 || i==1)return cost[i];
+        if(dp[i] != -1)return dp[i];
+
+        dp[i] = cost[i] + Math.min(minCost(i-1,cost,dp),minCost(i-2,cost,dp));
+        return dp[i];
     }
     public int minCostClimbingStairs(int[] cost) {
-        int n =  cost.length;
-        int [] dp = new int[n + 1];
+        int n = cost.length;
+        int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
-        return Math.min(minCost(cost,n-1,dp),minCost(cost,n-2,dp));//n-1 and n-2 isliye because question me given hai ki hum last me top floor ya usse pahele tak bhi ja sakte hai
+
+        return Math.min(minCost(n-1,cost,dp),minCost(n-2,cost,dp));
     }
 }
