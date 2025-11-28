88c0d51cbcc86327cf1298554e3bcc7bfa8102a0
Suyash Mishra <156529215+6394413745@users.noreply.github.com>
2025-11-28 23:42:27 +0530

    Time: 1 ms (99.89%), Space: 43 MB (28.47%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int [] freq = new int[128];
+        for(char c:s.toCharArray()){
+            freq[c]++;
+        }
+        int length = 0;
+        boolean hasOdd = false;
+        for(int i : freq){
+            if(i%2 == 0){
+                length += i;
+            }else{
+                length += i-1;
+                hasOdd = true;
+            }
+        }
+        if(hasOdd)length += 1;
+        return length;
+    }
+}
